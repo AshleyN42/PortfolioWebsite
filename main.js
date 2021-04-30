65174c1dc154afbcdd485ac06ad2c29bd0e03c35
Ashley Sisemore <ashley.sisemore@smail.astate.edu>
2021-04-30 00:50:11 -0500

    change

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 var displayedImage = document.querySelector('.displayed-img');
 var thumbBar = document.querySelector('.thumb-bar');
 
-var btn = document.querySelector('button');
-var overlay = document.querySelector('.overlay');
-
 /* Looping through images */
 
-for(let i = 1; i <= 5; i++) {
+for (let i = 1; i <= 5; i++) {
   const newImage = document.createElement('img');
   newImage.setAttribute('src', 'images/pic' + i + '.jpg');
   thumbBar.appendChild(newImage);
